96714a289f257135a59245d17a4d589618f23045
Hamlet Nadirian <gamlet.osmanov@gmail.com>
2021-12-30 14:49:52 +0200

    Delete acmp.ru directory

--- acmp.ru/Gulliver.java
+++
@@ -1,6 +0,0 @@
-https://acmp.ru/index.asp?main=task&id_task=773&ins=1#solution хотел попасть в топ "лучших попыток",где "В качестве критерия ранжирования лучших попыток служит размер кода.."
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args)
-    {Scanner in = new Scanner(System.in);byte a =(byte) in.nextByte();byte b = (byte)in .nextByte();int  r = ((a*a)*b);System.out.println(r);    }
-}
